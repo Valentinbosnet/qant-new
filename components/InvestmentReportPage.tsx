54b9e16752666d32f236ab18241c831a323b7564
Valentinbosnet <v.bosnet@icloud.com>
2025-03-18 11:34:54 +0100

    Delete components/InvestmentReportPage.tsx

--- components/InvestmentReportPage.tsx
+++
@@ -1,8 +0,0 @@
-export default function InvestmentReportPage() {
-  return (
-    <div className="p-8">
-      <h1 className="text-2xl font-bold">Investment Report</h1>
-      <p className="mt-4">This feature is currently under development.</p>
-    </div>
-  );
-}
