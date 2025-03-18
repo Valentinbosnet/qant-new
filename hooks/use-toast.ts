cf2ac67031a0bd2b3cd62b8aef8fe15a40b2983c
Valentinbosnet <v.bosnet@icloud.com>
2025-03-18 11:30:11 +0100

    Update use-toast.ts

--- hooks/use-toast.ts
+++ hooks/use-toast.ts
@@ -1,48 +1,8 @@
-"use client"
-
-import { useState } from "react"
-
-type ToastProps = {
-  title?: string
-  description?: string
-  type?: "default" | "success" | "error" | "warning"
-  duration?: number
-}
-
-type Toast = ToastProps & {
-  id: string
-}
-
-export function useToast() {
-  const [toasts, setToasts] = useState<Toast[]>([])
-
-  const toast = (props: ToastProps) => {
-    const id = Math.random().toString(36).substring(2, 9)
-    const newToast = {
-      id,
-      title: props.title || "",
-      description: props.description || "",
-      type: props.type || "default",
-      duration: props.duration || 5000,
-    }
-
-    setToasts((prevToasts) => [...prevToasts, newToast])
-
-    setTimeout(() => {
-      setToasts((prevToasts) => prevToasts.filter((toast) => toast.id !== id))
-    }, newToast.duration)
-
-    return id
-  }
-
-  const dismiss = (id: string) => {
-    setToasts((prevToasts) => prevToasts.filter((toast) => toast.id !== id))
-  }
-
+// Temporary placeholder
+export const useToast = () => {
   return {
-    toast,
-    dismiss,
-    toasts,
-  }
-}
-
+    toast: () => {},
+    dismiss: () => {},
+    toasts: []
+  };
+};
