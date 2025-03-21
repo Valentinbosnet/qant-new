9d6f6369852479d721a534f40789e92021e530fc
Valentin <v.bosnet@icloud.com>
2025-03-21 16:38:18 +0100

    Application prête pour le déploiement

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,9 @@
-// app/api/auth/[...nextauth]/route.ts
 import NextAuth from "next-auth"
 import { authOptions } from "@/lib/auth-options"
 
+// Exportez un gestionnaire qui utilise les options d'authentification
 const handler = NextAuth(authOptions)
+
+// Exportez les méthodes GET et POST pour le gestionnaire
 export { handler as GET, handler as POST }
+
